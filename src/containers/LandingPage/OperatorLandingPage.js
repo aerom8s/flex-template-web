c36130090617041845dd4f77cbb935dae0b6fad6
shawn <shawnantonucci2018@gmail.com>
2019-09-17 23:12:23 -0700

    added own your customers / simplified booking

--- src/containers/LandingPage/OperatorLandingPage.js
+++ src/containers/LandingPage/OperatorLandingPage.js
@@ -7,140 +7,182 @@ import { injectIntl, intlShape } from '../../util/reactIntl';
 import { isScrollingDisabled } from '../../ducks/UI.duck';
 import config from '../../config';
 import {
-    Page,
-    SectionHero,
-    SectionHowItWorks,
-    SectionLocations,
-    LayoutSingleColumn,
-    LayoutWrapperTopbar,
-    LayoutWrapperMain,
-    LayoutWrapperFooter,
-    Footer,
+  Page,
+  SectionHero,
+  SectionHowItWorks,
+  SectionLocations,
+  LayoutSingleColumn,
+  LayoutWrapperTopbar,
+  LayoutWrapperMain,
+  LayoutWrapperFooter,
+  Footer,
 } from '../../components';
 import { TopbarContainer } from '../../containers';
-import { Container, Image, Header, Button } from 'semantic-ui-react'
-import operatorHero from '../../assets/operatorHero.jpg'
+import { Container, Image, Header, Button } from 'semantic-ui-react';
+import operatorHero from '../../assets/operatorHero.jpg';
 
 import facebookImage from '../../assets/saunatimeFacebook-1200x630.jpg';
 import twitterImage from '../../assets/saunatimeTwitter-600x314.jpg';
-import tempImage from '../../assets/placeholder.png'
+import tempImage from '../../assets/placeholder.png';
+import charterCard from '../../assets/charterCard.png';
 import css from './OperatorLandingPage.css';
 
 export const LandingPageComponent = props => {
-    const { history, intl, location, scrollingDisabled } = props;
+  const { history, intl, location, scrollingDisabled } = props;
 
-    // Schema for search engines (helps them to understand what this page is about)
-    // http://schema.org
-    // We are using JSON-LD format
-    const siteTitle = config.siteTitle;
-    const schemaTitle = intl.formatMessage({ id: 'LandingPage.schemaTitle' }, { siteTitle });
-    const schemaDescription = intl.formatMessage({ id: 'LandingPage.schemaDescription' });
-    const schemaImage = `${config.canonicalRootURL}${facebookImage}`;
+  // Schema for search engines (helps them to understand what this page is about)
+  // http://schema.org
+  // We are using JSON-LD format
+  const siteTitle = config.siteTitle;
+  const schemaTitle = intl.formatMessage({ id: 'LandingPage.schemaTitle' }, { siteTitle });
+  const schemaDescription = intl.formatMessage({ id: 'LandingPage.schemaDescription' });
+  const schemaImage = `${config.canonicalRootURL}${facebookImage}`;
 
-    return (
-        <Page
-            className={css.root}
-            scrollingDisabled={scrollingDisabled}
-            contentType="website"
-            description={schemaDescription}
-            title={schemaTitle}
-            facebookImages={[{ url: facebookImage, width: 1200, height: 630 }]}
-            twitterImages={[
-                { url: `${config.canonicalRootURL}${twitterImage}`, width: 600, height: 314 },
-            ]}
-            schema={{
-                '@context': 'http://schema.org',
-                '@type': 'WebPage',
-                description: schemaDescription,
-                name: schemaTitle,
-                image: [schemaImage],
-            }}
-        >
-            <LayoutSingleColumn>
-                <LayoutWrapperTopbar>
-                    <TopbarContainer />
-                </LayoutWrapperTopbar>
-                <LayoutWrapperMain>
-                    <Container fluid>
-                        <Image className={css.operatorHeroStyle} src={operatorHero} fluid />
-                        <Header className={css.operatorHeroHeader} as='h1'>Private aviation, <br /> simplified.</Header>
-                        <p className={css.operatorHeroPara}>Aeromates powers the nation's top aviation operators, <br /> using technology to build
-                        the future of the industry.</p>
-                        <Button className={css.operatorHeroBtn} primary>Request a Demo</Button>
-                        <div className={css.operatorContainerDiv}>
-                            <div style={{ display: "flex", justifyContent: "center" }}>
-                                <h2 className={css.operatorHeader}>How does it work?</h2>
-                            </div>
-                            <div className={css.imageContainer}>
-                                <div className={css.cardContainer}>
-                                    <img className={css.imgStyle} src={tempImage} alt="student at desk" />
-                                    <h3>Create a custom profile for your company.</h3>
-                                </div>
-                                <div className={css.cardContainer}>
-                                    <img className={css.imgStyle} src={tempImage} alt="student at desk" />
-                                    <h3>Easily enter your aircraft, and <br /> and
-                                    set booking times for chartes, <br />
-                                        tours or training.</h3>
-                                </div>
-                                <div className={css.cardContainer}>
-                                    <img className={css.imgStyle} src={tempImage} alt="student at desk" />
-                                    <h3>That's it, you're open <br />
-                                        for Business.</h3>
-                                </div>
-                            </div>
-
-                            <div className={css.nextSection}>
-                                <div style={{ display: "flex", justifyContent: "center" }}>
-
-                                    <h2 className={css.operatorHeader2}>Why Aeromates?</h2>
-                                </div>
-
-                                <div className={css.customerContainer}>
-
-                                    <div className={css.customerCard}>
-                                        <div className={css.customerCardInfo}>
-                                            <h2 className={css.customerCardHeader}>Own your <br /> customers</h2>
-                                            <p className={css.customerCardPara}>Our best-in-class CRM comes standard, to <br />
-                                                make communicating with your customers <br />
-                                                easier than ever. Plus gain a greater insight <br />
-                                                into your business with our marketing <br />
-                                                analytics dashboard, at a level never before <br />
-                                                seen in the aviation industry.</p>
-                                        </div>
-                                        <img className={css.customerImage} src={tempImage} alt="tour pending / confirmed" />
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </Container>
-                </LayoutWrapperMain>
-                <LayoutWrapperFooter>
-                    <Footer />
-                </LayoutWrapperFooter>
-            </LayoutSingleColumn>
-        </Page>
-    );
+  return (
+    <Page
+      className={css.root}
+      scrollingDisabled={scrollingDisabled}
+      contentType="website"
+      description={schemaDescription}
+      title={schemaTitle}
+      facebookImages={[{ url: facebookImage, width: 1200, height: 630 }]}
+      twitterImages={[
+        { url: `${config.canonicalRootURL}${twitterImage}`, width: 600, height: 314 },
+      ]}
+      schema={{
+        '@context': 'http://schema.org',
+        '@type': 'WebPage',
+        description: schemaDescription,
+        name: schemaTitle,
+        image: [schemaImage],
+      }}
+    >
+      <LayoutSingleColumn>
+        <LayoutWrapperTopbar>
+          <TopbarContainer />
+        </LayoutWrapperTopbar>
+        <LayoutWrapperMain>
+          <Container className={css.containerClass} fluid>
+            <Image className={css.operatorHeroStyle} src={operatorHero} fluid />
+            <Header className={css.operatorHeroHeader} as="h1">
+              Private aviation, <br /> simplified.
+            </Header>
+            <p className={css.operatorHeroPara}>
+              Aeromates powers the nation's top aviation operators, <br /> using technology to build
+              the future of the industry.
+            </p>
+            <Button className={css.operatorHeroBtn} primary>
+              Request a Demo
+            </Button>
+            <div className={css.operatorContainerDiv}>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <h2 className={css.operatorHeader}> How does it work ? </h2>
+              </div>
+              <div className={css.imageContainer}>
+                <div className={css.cardContainer}>
+                  <img className={css.imgStyle} src={tempImage} alt="student at desk" />
+                  <p className={css.cardPara}>
+                    Create a custom profile for <br /> your company.
+                  </p>
+                </div>
+                <div className={css.cardContainer}>
+                  <img className={css.imgStyle} src={tempImage} alt="student at desk" />
+                  <p className={css.cardPara}>
+                    Easily enter your aircraft, and <br /> and set booking times for charters,{' '}
+                    <br />
+                    tours or training.
+                  </p>
+                </div>
+                <div className={css.cardContainer}>
+                  <img className={css.imgStyle} src={tempImage} alt="student at desk" />
+                  <p className={css.cardPara}>
+                    That's it, you're open <br />
+                    for Business.
+                  </p>
+                </div>
+              </div>
+              <div className={css.nextSection}>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                  <h2 className={css.operatorHeader2}> Why Aeromates ? </h2>
+                </div>
+                <div className={css.customerContainer}>
+                  <div className={css.customerCardNext}>
+                    <div className={css.customerCardInfo}>
+                      <h2 className={css.customerCardHeader}>
+                        Own your <br /> customers
+                      </h2>
+                      <p className={css.customerCardPara}>
+                        Our best - in -class CRM comes standard, to <br />
+                        make communicating with your customers <br />
+                        easier than ever.Plus gain a greater insight <br />
+                        into your business with our marketing <br />
+                        analytics dashboard, at a level never before <br />
+                        seen in the aviation industry.
+                      </p>
+                    </div>
+                    {/* <img
+                      className={css.customerImage}
+                      src={tempImage}
+                      alt="tour pending / confirmed"
+                    /> */}
+                    <div className={css.tourPendingContainer}>
+                      <div className={css.tourPending}>Tour Pending</div>
+                      <div className={css.tourContainer}>
+                          <img className={css.tourProfileImg} src={tempImage} alt="profile picture"/>
+                          <img className={css.tourArrows} src={tempImage} alt="arrows pointing to profile and charter company"/>
+                          <img className={css.tourCompany} src={tempImage} alt="company profile"/>
+                      </div>
+                      <div className={css.tourConfirmed}>Tour Confirmed</div>
+                    </div>
+                  </div>
+                  <div className={css.customerCardNext}>
+                    <img
+                      className={css.charterImage}
+                      src={charterCard}
+                      alt="charter card example"
+                    />
+                    <div className={css.customerCardInfo}>
+                      <h2 className={css.customerCardHeader}>
+                        Simplified <br /> booking
+                      </h2>
+                      <p className={css.customerCardPara}>
+                        Allow your customers to instantly book or <br />
+                        request flights, boosting your conversion rate <br />
+                        while maintaining complete control over the <br />
+                        flights you accept.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </LayoutWrapperMain>
+        <LayoutWrapperFooter>
+          <Footer />
+        </LayoutWrapperFooter>
+      </LayoutSingleColumn>
+    </Page>
+  );
 };
 
 const { bool, object } = PropTypes;
 
 LandingPageComponent.propTypes = {
-    scrollingDisabled: bool.isRequired,
+  scrollingDisabled: bool.isRequired,
 
-    // from withRouter
-    history: object.isRequired,
-    location: object.isRequired,
+  // from withRouter
+  history: object.isRequired,
+  location: object.isRequired,
 
-    // from injectIntl
-    intl: intlShape.isRequired,
+  // from injectIntl
+  intl: intlShape.isRequired,
 };
 
 const mapStateToProps = state => {
-    return {
-        scrollingDisabled: isScrollingDisabled(state),
-    };
+  return {
+    scrollingDisabled: isScrollingDisabled(state),
+  };
 };
 
 // Note: it is important that the withRouter HOC is **outside** the
@@ -150,13 +192,13 @@ const mapStateToProps = state => {
 //
 // See: https://github.com/ReactTraining/react-router/issues/4671
 const LandingPage = compose(
-    withRouter,
-    connect(mapStateToProps),
-    injectIntl
+  withRouter,
+  connect(mapStateToProps),
+  injectIntl
 )(LandingPageComponent);
 
 export default LandingPage;
 
 const fontcolor = {
-    color: 'white',
+  color: 'white',
 };
